1a42ccfb5411e3dc3775d5c64f88d045fe1de749
OscarEd08 <oscar.pairazaman2@unmsm.edu.pe>
2022-11-09 12:39:02 -0500

    Primer desarrollo - Agregando burguer menu

---
+++ Desarrollo/ACU_FE/App_Comedor_UNMSM/app/src/main/java/com/example/app_comedor_unmsm/ErrorFragment.kt
@@ -0,0 +1,11 @@
+package com.example.app_comedor_unmsm
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ErrorFragment : Fragment(R.layout.fragment_error) {
+
+}
