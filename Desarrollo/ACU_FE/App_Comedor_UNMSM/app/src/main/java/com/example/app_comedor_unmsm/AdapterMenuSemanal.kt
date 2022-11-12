552dac1efefd316f22b40dffdf97d11a73571389
OscarEd08 <oscar.pairazaman2@unmsm.edu.pe>
2022-11-12 10:32:43 -0500

    Agregando la lista del menu semanal

---
+++ Desarrollo/ACU_FE/App_Comedor_UNMSM/app/src/main/java/com/example/app_comedor_unmsm/AdapterMenuSemanal.kt
@@ -0,0 +1,45 @@
+package com.example.app_comedor_unmsm
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class AdapterMenuSemanal: RecyclerView.Adapter<AdapterMenuSemanal.ViewHolder>() {
+    val dates = arrayOf("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado")
+    val food = arrayOf("Pollo a la brasa", "Lomo Saltado", "Tacacho con Cecina",
+        "Arroz con Pollo", "Arroz a la jardinera", "Pure de papas")
+    val images = intArrayOf(R.drawable.pollo,R.drawable.pollo,
+        R.drawable.tacacho,R.drawable.arrozpollo,
+        R.drawable.jardinera,R.drawable.pure)
+
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val v = LayoutInflater.from(viewGroup.context).inflate(R.layout.lista_menu_semanal, viewGroup,false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, i: Int) {
+        viewHolder.itemDate.text = dates[i]
+        viewHolder.itemFood.text = food[i]
+        viewHolder.itemImage.setImageResource(images[i])
+    }
+
+    override fun getItemCount(): Int {
+        return dates.size
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var itemImage: ImageView
+        var itemDate: TextView
+        var itemFood: TextView
+
+        init {
+            itemImage = itemView.findViewById(R.id.item_image)
+            itemDate = itemView.findViewById(R.id.item_date)
+            itemFood = itemView.findViewById(R.id.item_food)
+        }
+    }
+}
--- Desarrollo/ACU_FE/App_Comedor_UNMSM/app/src/main/java/com/example/app_comedor_unmsm/MenuSemanalFragment.kt
+++ Desarrollo/ACU_FE/App_Comedor_UNMSM/app/src/main/java/com/example/app_comedor_unmsm/MenuSemanalFragment.kt
@@ -5,7 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_menu_semanal.*
 
-class MenuSemanalFragment : Fragment(R.layout.fragment_menu_semanal) {
+class MenuSemanalFragment : Fragment(R.layout.lista_menu_semanal) {
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<AdapterMenuSemanal.ViewHolder>? = null
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_menu_semanal, container, false)
+    }
+
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(activity)
+            adapter = AdapterMenuSemanal()
+        }
+    }
 }
